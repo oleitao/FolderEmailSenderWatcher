3740a0dbf1c8f760e4ca1647b188ee99afef69f6
oleitao <olivleitao@gmail.com>
2022-12-13 20:58:23 +0000

    Added settings panel (database connection/sender email setup)

---
+++ FolderEmailSenderWatcher/FormDBSettingConnection.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace FolderEmailSenderWatcher
+{
+    partial class FormDBSettingConnection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblSmtpAddress = new System.Windows.Forms.Label();
+            this.txtSmtpAddress = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.ckSSL = new System.Windows.Forms.CheckBox();
+            this.txtFromEmail = new System.Windows.Forms.TextBox();
+            this.lblFromEmail = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.lblBody = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtConnectionString);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(551, 75);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Database Connection";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(116, 26);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(429, 20);
+            this.txtConnectionString.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection String:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(489, 327);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(408, 327);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtPassword);
+            this.groupBox2.Controls.Add(this.lblPassword);
+            this.groupBox2.Controls.Add(this.lblBody);
+            this.groupBox2.Controls.Add(this.txtBody);
+            this.groupBox2.Controls.Add(this.txtFromEmail);
+            this.groupBox2.Controls.Add(this.lblFromEmail);
+            this.groupBox2.Controls.Add(this.ckSSL);
+            this.groupBox2.Controls.Add(this.txtPort);
+            this.groupBox2.Controls.Add(this.lblPort);
+            this.groupBox2.Controls.Add(this.txtSmtpAddress);
+            this.groupBox2.Controls.Add(this.lblSmtpAddress);
+            this.groupBox2.Location = new System.Drawing.Point(13, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(551, 226);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sender Email";
+            // 
+            // lblSmtpAddress
+            // 
+            this.lblSmtpAddress.AutoSize = true;
+            this.lblSmtpAddress.Location = new System.Drawing.Point(14, 24);
+            this.lblSmtpAddress.Name = "lblSmtpAddress";
+            this.lblSmtpAddress.Size = new System.Drawing.Size(81, 13);
+            this.lblSmtpAddress.TabIndex = 0;
+            this.lblSmtpAddress.Text = "SMTP Address:";
+            // 
+            // txtSmtpAddress
+            // 
+            this.txtSmtpAddress.Location = new System.Drawing.Point(101, 21);
+            this.txtSmtpAddress.Name = "txtSmtpAddress";
+            this.txtSmtpAddress.Size = new System.Drawing.Size(145, 20);
+            this.txtSmtpAddress.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(476, 21);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(65, 20);
+            this.txtPort.TabIndex = 3;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(441, 24);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(29, 13);
+            this.lblPort.TabIndex = 2;
+            this.lblPort.Text = "Port:";
+            // 
+            // ckSSL
+            // 
+            this.ckSSL.AutoSize = true;
+            this.ckSSL.Location = new System.Drawing.Point(295, 24);
+            this.ckSSL.Name = "ckSSL";
+            this.ckSSL.Size = new System.Drawing.Size(82, 17);
+            this.ckSSL.TabIndex = 4;
+            this.ckSSL.Text = "Enable SSL";
+            this.ckSSL.UseVisualStyleBackColor = true;
+            // 
+            // txtFromEmail
+            // 
+            this.txtFromEmail.Location = new System.Drawing.Point(125, 57);
+            this.txtFromEmail.Name = "txtFromEmail";
+            this.txtFromEmail.Size = new System.Drawing.Size(193, 20);
+            this.txtFromEmail.TabIndex = 6;
+            // 
+            // lblFromEmail
+            // 
+            this.lblFromEmail.AutoSize = true;
+            this.lblFromEmail.Location = new System.Drawing.Point(14, 60);
+            this.lblFromEmail.Name = "lblFromEmail";
+            this.lblFromEmail.Size = new System.Drawing.Size(102, 13);
+            this.lblFromEmail.TabIndex = 5;
+            this.lblFromEmail.Text = "From Email Address:";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(7, 123);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(538, 97);
+            this.txtBody.TabIndex = 7;
+            // 
+            // lblBody
+            // 
+            this.lblBody.AutoSize = true;
+            this.lblBody.Location = new System.Drawing.Point(7, 104);
+            this.lblBody.Name = "lblBody";
+            this.lblBody.Size = new System.Drawing.Size(67, 13);
+            this.lblBody.TabIndex = 8;
+            this.lblBody.Text = "HTML Body:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(386, 57);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(155, 20);
+            this.txtPassword.TabIndex = 10;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(324, 60);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 9;
+            this.lblPassword.Text = "Password:";
+            // 
+            // FormDBSettingConnection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 362);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormDBSettingConnection";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Settings";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtSmtpAddress;
+        private System.Windows.Forms.Label lblSmtpAddress;
+        private System.Windows.Forms.TextBox txtFromEmail;
+        private System.Windows.Forms.Label lblFromEmail;
+        private System.Windows.Forms.CheckBox ckSSL;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblBody;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
---
+++ FolderEmailSenderWatcher/FormDBSettingConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FolderEmailSenderWatcher
+{
+    public partial class FormDBSettingConnection : Form
+    {
+        public FormDBSettingConnection()
+        {
+            InitializeComponent();
+
+            txtConnectionString.Text = Properties.Settings.Default.ConnectionString;
+
+            txtSmtpAddress.Text = Properties.Settings.Default.SMTPAddress;
+            ckSSL.Checked = Properties.Settings.Default.EnableSSL;
+            txtPort.Text = Properties.Settings.Default.PortNumber.ToString();
+            txtFromEmail.Text = Properties.Settings.Default.FromEmail;
+            txtPassword.Text = Properties.Settings.Default.Password;
+            txtBody.Text = Properties.Settings.Default.Body;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.ConnectionString = txtConnectionString.Text;
+
+            Properties.Settings.Default.SMTPAddress = txtSmtpAddress.Text;
+            Properties.Settings.Default.Password = txtPassword.Text;
+            Properties.Settings.Default.EnableSSL = ckSSL.Checked;
+            int.TryParse(txtPort.Text, out int port);
+            Properties.Settings.Default.PortNumber = port;
+            Properties.Settings.Default.FromEmail = txtFromEmail.Text;
+            Properties.Settings.Default.Body = txtBody.Text;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
